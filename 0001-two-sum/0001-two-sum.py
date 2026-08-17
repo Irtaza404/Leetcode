534b1f60f5b63b79049867ac4333d935eb564c5e
Muhammad Irtaza <90450152+Irtaza404@users.noreply.github.com>
2026-08-18 00:22:40 +0500

    Time: 0 ms (100%), Space: 13.2 MB (36.45%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        seen={}
+        for i,n in enumerate(nums):
+            sub=target-n
+            if n in seen:
+                return [seen[n],i]
+            else:
+                seen[sub]=i

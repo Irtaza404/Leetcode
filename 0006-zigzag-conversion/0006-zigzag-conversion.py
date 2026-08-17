22f0527e2cbd9d97c825cd137576e5f77d6c05a2
Muhammad Irtaza <90450152+Irtaza404@users.noreply.github.com>
2026-08-18 00:25:26 +0500

    Time: 12 ms (35.77%), Space: 19.3 MB (46.48%) - LeetHub

---
+++ 0006-zigzag-conversion/0006-zigzag-conversion.py
@@ -0,0 +1,22 @@
+class Solution:
+    def convert(self,s, numRows):
+        if numRows == 1:
+            return s
+        buckets = [""] * numRows
+        current_row = 0
+        direction = -1
+        print(buckets)
+        for char in s:
+            if current_row >= numRows - 1:
+                direction = 1
+            elif current_row == 0:
+                direction = -1
+
+            buckets[current_row] += char
+
+            if direction == -1:
+                current_row += 1
+            else:
+                current_row -= 1
+        print(buckets)
+        return "".join(buckets)

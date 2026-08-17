1ebba9f55260ac4e4efcf0f8052a2f6aab7c1c91
Muhammad Irtaza <90450152+Irtaza404@users.noreply.github.com>
2026-08-18 00:29:49 +0500

    Time: 689 ms (7.52%), Space: 19.3 MB (53.5%) - LeetHub

---
+++ 0018-4sum/0018-4sum.py
@@ -0,0 +1,19 @@
+class Solution:
+    def fourSum(self, nums: List[int], target: int) -> List[List[int]]:
+        nums.sort()
+        res=[]
+        for i in range(len(nums)):
+            for j in range(i+1,len(nums)-1):
+                l,r=j+1,len(nums)-1
+                while l<r:
+                    total=nums[i]+nums[j]+nums[l]+nums[r]
+                    if total==target:
+                        t=[nums[i],nums[j],nums[l],nums[r]]
+                        if t not in res:
+                            res.append(t)
+                    
+                    if total<target:
+                        l+=1
+                    else:
+                        r-=1
+        return res

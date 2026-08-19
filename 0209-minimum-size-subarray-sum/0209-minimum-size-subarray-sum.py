3c2789d96781597a970dd3deaff3e58e36602f83
Muhammad Irtaza <90450152+Irtaza404@users.noreply.github.com>
2026-08-20 00:03:24 +0500

    Time: 14 ms (90.37%), Space: 30.6 MB (16.42%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.py
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.py
@@ -1,19 +1,20 @@
 class Solution:
     def minSubArrayLen(self, target: int, nums: List[int]) -> int:
-        window_sum=windowstart=size=0
-
-        n=len(nums)
-        min_len=n+1
+        window_sum = 0
+        window_start = 0
+        n = len(nums)
+        min_len = n + 1
+        
         for i in range(n):
-            window_sum+=nums[i]
-            size+=1
-            if window_sum>=target:
-                while window_sum>=target:
-                    window_sum-=nums[windowstart]
-                    windowstart+=1
-                    size-=1
-                min_len=min(min_len,size+1)
+            window_sum += nums[i]
+            
+            # We don't need the 'if', we can just use the 'while'!
+            while window_sum >= target:
+                # 1. Record the size BEFORE we shrink it
+                min_len = min(min_len, i - window_start + 1)
+                
+                # 2. Shrink it
+                window_sum -= nums[window_start]
+                window_start += 1
 
-        return min_len if min_len!=n+1 else 0
-
-
+        return min_len if min_len != n + 1 else 0
